9f7cda60625ed6e10d2f4cbf1050cb7de88a00ae
Luke <ryanl9@uni.coventry.ac.uk>
2018-03-28 20:13:32 +0100

    load_urls_from_file, load_page, scrape_page added to url_utilities.py

--- page_spider.py
+++ page_spider.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-db", "--database", help="SQLite File Name")
     parser.add_argument("-i", "--input", help="File containing urls to read")
-    args = parser.parge_args()
+    args = parser.parse_args()
     database_file = args.database
     input_file = args.input
     main(database=database_file, url_list_file=input_file)
