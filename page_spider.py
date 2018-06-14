4bfd6049c6057f2bb6fdd94cdd180da484be2d65
James Vickery <dev@jamesvickery.net>
2018-06-14 22:39:09 +0100

    Error checking and requirements file

--- page_spider.py
+++ page_spider.py
@@ -1,5 +1,6 @@
-import os
 import argparse
+import os
+
 from utilities import url_utilities, database_utilities
 
 
@@ -15,7 +16,7 @@ def main(database: str, url_list_file: str):
         big_word_list.extend(words)
 
     # database code
-    os.chdir(os.path.dirname(__file__))
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
     path = os.path.join(os.getcwd(), "words.db")
     database_utilities.create_database(database_path=path)
     database_utilities.save_words_to_database(database_path=path, words_list=big_word_list)
@@ -26,6 +27,13 @@ if __name__ == "__main__":
     parser.add_argument("-db", "--database", help="SQLite File Name")
     parser.add_argument("-i", "--input", help="File containing urls to read")
     args = parser.parse_args()
+
+    if not args.database:
+        raise ValueError("Missing database (-db) argument")
+    if not args.input:
+        raise ValueError("Missing input (-i) argument")
+
     database_file = args.database
     input_file = args.input
+
     main(database=database_file, url_list_file=input_file)
